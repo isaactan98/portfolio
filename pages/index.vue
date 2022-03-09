5056ce92a4d3776d8ead3a2641e317a1cd350d08
Isaac Tan <iztan98@gmail.com>
2022-03-09 15:26:56 +0800

    update: toggle button for dark mode

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,29 @@
       h-[100vh]
       bg-gradient-to-tr
       from-indigo-100
-      dark:from-slate-800 dark:to-slate-700
+      dark:from-slate-800 dark:to-slate-500
     "
-  ></div>
+  >
+    <ChangeThemeButton />
+  </div>
 </template>
 
 <script>
+import ChangeThemeButton from "~/components/ChangeThemeButton.vue";
 export default {
+  components: { ChangeThemeButton },
   name: "IndexPage",
+  mounted() {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (
+      localStorage.getItem("tcvDarkMode") === "true" ||
+      (!("tcvDarkMode" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  },
 };
 </script>
