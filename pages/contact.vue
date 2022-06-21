b2d4c4a569397d48190ba37e6b71dbada5a483ae
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-06-21 21:55:01 +0800

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,51 +1,49 @@
 <template>
   <div class="grid p-5 sm:m-auto">
     <div class="min-h-screen">
-      <div
-        class="
-          p-5
-          sm:p-10
-          rounded-lg
-          shadow-lg
-          bg-gray-100/30
-          dark:bg-zinc-500/30
-          backdrop-blur-lg
-        "
-      >
-        <div class="text-zinc-800 dark:text-white font-bold text-2xl">
-          Contact Me
-        </div>
-        <form ref="form" @submit.prevent="sendEmail">
-          <div class="py-3">
-            <div class="my-3">
-              <input
-                name="title"
-                type="text"
-                class="p-2 border w-full shadow-md rounded-md"
-                placeholder="A title here, cuz why not"
-              />
-            </div>
-            <div class="my-3">
-              <input
-                name="email"
-                type="email"
-                class="p-2 border w-full shadow-md rounded-md"
-                placeholder="Your email, so I can contact you back."
-              />
-            </div>
-            <div class="my-3">
-              <textarea
-                name="message"
-                class="p-2 border w-full shadow-md rounded-md"
-                placeholder="Write your message here."
-              ></textarea>
-            </div>
-            <div class="my-3 flex justify-end">
-              <button type="submit" id="submit" class="btn">Submit Here</button>
-            </div>
-          </div>
-        </form>
+      <div class="text-zinc-800 dark:text-white font-bold text-2xl">
+        Contact Me
       </div>
+      <form ref="form" @submit.prevent="sendEmail">
+        <div class="p-5 mt-3 mockup-code bg-slate-50 dark:bg-zinc-700">
+          <pre data-prefix="1" class="my-2"><code><input
+              id="title"
+              name="title"
+              type="text"
+              class="w-2/3 bg-transparent outline-none dark:text-white"
+              placeholder="Click here to enter." autofocus autocomplete="off"
+            /></code></pre>
+          <pre data-prefix="2" class="my-2"><code><input
+              id="email"
+              name="email"
+              type="email"
+              class="w-2/3 bg-transparent outline-none dark:text-white"
+              placeholder="Enter your email here." autocomplete="off"
+            /></code></pre>
+          <pre data-prefix="3"><code></code></pre>
+          <pre data-prefix="" class="my-2"><code><textarea
+              id="message"
+              name="message"
+              class="w-2/3 bg-transparent outline-none dark:text-white"
+              placeholder="Write your message here."
+            ></textarea></code></pre>
+          <pre
+            data-prefix=">"
+            class="text-green-500"
+            id="show_msg"
+          ><code>Done!</code></pre>
+          <!--  -->
+          <div class="my-3 flex justify-end">
+            <button
+              type="submit"
+              id="submit"
+              class="btn bg-zinc-500 hover:text-white"
+            >
+              Submit Here
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -67,10 +65,16 @@ export default {
     if (e != null) {
       e.parentElement.removeChild(e);
     }
+    const show_msg = document.getElementById("show_msg");
+    show_msg.classList.add("hidden");
   },
   methods: {
     sendEmail() {
       const submit_btn = document.getElementById("submit");
+      const input_title = document.getElementById("title");
+      const input_email = document.getElementById("email");
+      const input_message = document.getElementById("message");
+      const show_msg = document.getElementById("show_msg");
 
       submit_btn.classList.add("loading");
 
@@ -83,7 +87,14 @@ export default {
         )
         .then((s) => {
           submit_btn.classList.remove("loading");
-          console.log(s);
+          input_title.value = "";
+          input_email.value = "";
+          input_message.value = "";
+          show_msg.classList.remove("hidden");
+
+          setTimeout(() => {
+            show_msg.classList.add("hidden");
+          }, 10000);
         })
         .catch((e) => {
           console.log(e);
