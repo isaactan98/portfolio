a2dd2cd90f5e16ec37d6a02c1e6f3cb211b1a293
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-08-01 12:18:15 +0800

    update: added repo to work page

--- pages/works.vue
+++ pages/works.vue
@@ -1,6 +1,37 @@
 <template>
-  <div class="grid p-2 sm:m-auto sm:w-2/3">
-    <div class="min-h-screen"></div>
+  <div class="grid p-5 w-full sm:m-auto sm:max-w-3xl">
+    <div class="min-h-screen">
+      <div class="flex mb-4">
+        <h1 class="dark:text-gray-300 text-3xl">Github Repo</h1>
+      </div>
+
+      <div class="grid gap-4 grid-cols-1 sm:grid-cols-2" v-if="loading == true">
+
+        <div class="card bg-gray-100 shadow-xl" v-for="x in 6" :key="x">
+          <div class="card-body bg-zinc-500 animate-pulse">
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 mb-10 sm:mb-0" v-else>
+
+        <div class="card dark:bg-gray-100 shadow-xl" v-for="x, key in repo" :key="key">
+          <div class="card-body">
+            <h2 class="card-title">{{ x.name }}</h2>
+            <p>{{ x.description ?? 'No Description' }}</p>
+            <div class="card-actions justify-end">
+              <a :href="x.html_url" target="_blank" class="btn btn-sm btn-primary">View Repo</a>
+            </div>
+            <div class="card-actions">
+              <span class="badge badge-primary">{{ x.language }}</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +40,26 @@ export default {
   head: {
     title: "Works - Isaac Tan | Portfolio",
   },
+  data() {
+    return {
+      repo: [],
+      loading: true
+    }
+  },
   mounted() {
+    const url = "https://api.github.com/users/isaactan98/repos";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(element => {
+          this.loading = false;
+          if (element.private == false) {
+            this.repo.push(element);
+          }
+        });
+        console.log(this.repo);
+      }).catch(e => console.log(e));
   },
 };
 </script>
