7294b1674148a9eec47febb86bf98a0317342fff
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-09-20 12:50:49 +0800

    update: tech stack with line

--- pages/index.vue
+++ pages/index.vue
@@ -109,7 +109,7 @@
           </div>
           <div class="pl-14 -indent-14 dark:text-white">
             <span class="font-bold mr-4">2022</span>
-            Studying Computer Science in Software Engineering at UTM
+            Fresh Graduated from Computer Science in Software Engineering at UTM
           </div>
         </div>
         <!--  -->
@@ -126,11 +126,13 @@
           >
             Tech Stack
           </h3>
-          <div v-if="techStack != null" class="">
+          <div v-if="techStack != null" class="relative">
             <div v-for="(a, k) in techStack" :key="k" class="mb-4">
               <div
                 class="flex"
-                :class="a.type == 'language' ? 'justify-start' : 'justify-end'"
+                :class="
+                  a.type == 'language' ? 'justify-start' : 'justify-end mt-4'
+                "
               >
                 <div class="flex">
                   <div class="mr-4">
@@ -140,6 +142,84 @@
                     <div class="font-bold dark:text-white">{{ a.name }}</div>
                   </div>
                 </div>
+              </div>
+            </div>
+
+            <div
+              class="
+                absolute
+                border border-zinc-500
+                top-0
+                left-1/2
+                md:left-1/3
+                h-1/2
+              "
+              id="lang_section"
+            >
+              <div
+                class="absolute top-1/2 ml-8 dark:text-white text-2xl font-bold"
+              >
+                Language
+              </div>
+            </div>
+
+            <div
+              class="
+                block
+                absolute
+                h-[10%]
+                md:h-[5%]
+                w-1/4
+                left-1/2
+                md:left-1/3 md:border-b-2
+                border-l-2 border-zinc-500
+                md:rounded-bl-3xl
+              "
+              id="first_section"
+            ></div>
+
+            <div
+              class="
+                hidden
+                md:block
+                absolute
+                h-[6%]
+                left-1/2
+                md:right-1/3 md:border-t-2
+                border-r-2 border-zinc-500
+                md:rounded-tr-3xl
+              "
+              id="second_section"
+            ></div>
+
+            <div
+              class="
+                absolute
+                border-r-zinc-500
+                border-l-0
+                border-spacing-1
+                border-2
+                border-t-0
+                border-b-0
+                left-1/2
+                md:right-1/3
+                top-1/2
+                sm:top-1/3
+              "
+              id="framework_section"
+            >
+              <div
+                class="
+                  dark:text-white
+                  text-2xl
+                  font-bold
+                  absolute
+                  top-1/2
+                  right-0
+                  mr-8
+                "
+              >
+                Framework
               </div>
             </div>
           </div>
@@ -166,6 +246,10 @@ export default {
       lmS: null,
       aboutMe: null,
       techStack: null,
+      counter: {
+        lang: 0,
+        framework: 0,
+      },
     };
   },
 
@@ -174,9 +258,57 @@ export default {
       this.aboutMe = entry;
     });
 
-    client.getEntry("56ZOVSw8Spo7dvYSGMYlsp").then((entry) => {
-      this.techStack = entry.fields.techStack.stack;
-    });
+    client
+      .getEntry("56ZOVSw8Spo7dvYSGMYlsp")
+      .then((entry) => {
+        this.techStack = entry.fields.techStack.stack;
+
+        this.techStack.forEach((a) => {
+          if (a.type == "language") {
+            this.counter.lang += 1;
+          } else {
+            this.counter.framework += 1;
+          }
+        });
+      })
+      .then(() => {
+        const lang_section = document.getElementById("lang_section");
+        const framework_section = document.getElementById("framework_section");
+
+        lang_section.style.minHeight = `${
+          (this.counter.lang / (this.counter.lang + this.counter.framework)) *
+            100 -
+          5
+        }%`;
+
+        framework_section.style.top = `${
+          (this.counter.lang / (this.counter.lang + this.counter.framework)) *
+            100 +
+          5
+        }%`;
+
+        framework_section.style.minHeight = `${
+          (this.counter.framework /
+            (this.counter.lang + this.counter.framework)) *
+            100 -
+          5
+        }%`;
+
+        const first_section = document.getElementById("first_section");
+        const second_section = document.getElementById("second_section");
+
+        first_section.style.top = `${
+          (this.counter.lang / (this.counter.lang + this.counter.framework)) *
+            100 -
+          5
+        }%`;
+
+        second_section.style.top = `${
+          (this.counter.lang / (this.counter.lang + this.counter.framework)) *
+            100 -
+          0.25
+        }%`;
+      });
   },
 };
 </script>
